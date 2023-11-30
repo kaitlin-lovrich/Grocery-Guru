ede0babea66b53f972168d98ac98b0fc16b23548
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-11-30 18:30:38 +0000

    Search component functionality working

--- client/src/pages/BrowseRecipes.tsx
+++ client/src/pages/BrowseRecipes.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function BrowseRecipes() {
-  const [allRecipes, setAllRecipes] = useState<Recipe[]>();
+  const [allRecipes, setAllRecipes] = useState<Recipe[]>([]);
 
   useEffect(() => {
     async function loadBrowseRecipes() {
@@ -13,8 +13,56 @@ export default function BrowseRecipes() {
       setAllRecipes(recipes);
     }
     loadBrowseRecipes();
-  }, []);
+  }, [setAllRecipes]);
 
+  return (
+    <>
+      <SearchRecipesComponent allRecipes={allRecipes} />
+    </>
+  );
+}
+type SearchComponentProps = {
+  allRecipes: Recipe[];
+};
+
+function SearchRecipesComponent({ allRecipes }: SearchComponentProps) {
+  const [input, setInput] = useState('');
+
+  const inputList = allRecipes.filter((recipe) =>
+    recipe.title.toLowerCase().match(input)
+  );
+
+  return (
+    <>
+      <div className="search-bar-container">
+        <div className="search-bar">
+          <SearchBar input={input} onChangeInput={setInput} />
+        </div>
+      </div>
+      <RecipeList allRecipes={inputList} />
+    </>
+  );
+}
+
+type SearchBarProps = {
+  input: string;
+  onChangeInput: (value: string) => void;
+};
+
+function SearchBar({ input, onChangeInput }: SearchBarProps) {
+  return (
+    <input
+      value={input}
+      onChange={(e) => onChangeInput(e.currentTarget.value)}
+      className="input"></input>
+  );
+}
+
+type RecipeListProps = {
+  allRecipes: Recipe[];
+};
+
+function RecipeList({ allRecipes }: RecipeListProps) {
   return (
     <div className="browse-recipes-page">
       <h1 className="page-heading">Browse Recipes</h1>
