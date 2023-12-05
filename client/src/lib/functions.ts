17251c2ce3c0cf9243aae7359fb0a40897801949
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-12-05 01:45:21 +0000

    Format functions WIP

--- client/src/lib/functions.ts
+++ client/src/lib/functions.ts
@@ -1,5 +1,50 @@
 import { RecipeIngredient } from './dataTypes';
 
 export function formatRecipeIngredient(ingredient: RecipeIngredient) {
-  return `${ingredient.name} ${ingredient.quantity}`;
+  if (
+    ingredient.measurement === 'ounce' ||
+    (ingredient.measurement === 'slice' && ingredient.quantity <= 1)
+  ) {
+    return `${ingredient.quantity} ${ingredient.measurement} ${ingredient.name}`;
+  } else if (
+    ingredient.measurement === 'ounce' ||
+    (ingredient.measurement === 'slice' &&
+      ingredient.quantity >= 1 &&
+      ingredient.name.slice(-1) === 's')
+  ) {
+    return `${ingredient.quantity} ${ingredient.measurement} ${ingredient.name}`;
+  } else if (
+    ingredient.measurement === 'ounce' ||
+    (ingredient.measurement === 'slice' && ingredient.quantity >= 1)
+  ) {
+    return `${ingredient.quantity} ${ingredient.measurement}s ${ingredient.name}`;
+  } else if (
+    !ingredient.measurement.length ||
+    (!ingredient.packageType.length && ingredient.quantity <= 1)
+  ) {
+    return `${ingredient.quantity} ${ingredient.name}`;
+  } else if (
+    !ingredient.measurement.length ||
+    (!ingredient.packageType.length &&
+      ingredient.quantity >= 1 &&
+      ingredient.name.slice(-1) === 's')
+  ) {
+    return `${ingredient.quantity} ${ingredient.name}`;
+  } else if (
+    !ingredient.measurement.length ||
+    (!ingredient.packageType.length && ingredient.quantity >= 1)
+  ) {
+    return `${ingredient.quantity} ${ingredient.name}s`;
+  }
+  // else if (!ingredient.measurement.length && ingredient.quantity < 1) {
+  //   return `${ingredient.quantity} ${ingredient.name}`;
+  // } else if (
+  //   !ingredient.measurement.length &&
+  //   ingredient.quantity > 1 &&
+  //   ingredient.name.slice(-1) === 's'
+  // ) {
+  //   return `${ingredient.quantity} ${ingredient.name}`;
+  // } else if (!ingredient.measurement.length && ingredient.quantity > 1) {
+  //   return `${ingredient.quantity} ${ingredient.name}`;
+  // }
 }
